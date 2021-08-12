6b3947c8473be156f7c39aeb342e1054a82faec2
neomarwilliams <neoma.rae@gmail.com>
2021-08-11 19:25:27 -0700

    builing Alfred quotes

---
+++ 01-java-fundamentals/01-alfred-bot/AlfredQuotes.java
@@ -0,0 +1,9 @@
+import java.util.Date 
+public class AlfredQuotes {
+    public String basicGreeting() {
+        return "Hello, lovely to see you. How are you?";
+    }
+    public String guestGreeting(String name, String dayPeriod) {
+        System.out.println("%s, %s. Lovely to see you.");
+    }
+}
---
+++ 01-java-fundamentals/01-alfred-bot/AlfredTest.java
@@ -0,0 +1,5 @@
+public class AlfredTest {
+    public static void main(String[] args){
+        
+    }
+}
